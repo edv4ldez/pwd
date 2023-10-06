596b5711e30b9a617883722fdc0097cc5b7fb968
edv4ldez <ed.9820v@gmail.com>
2023-10-06 13:26:55 -0600

    UI improved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,14 +12,14 @@ export default function Home() {
         <h1 className={styles.title}>Agent P</h1>
         <p className={styles.text}>Passwords Generator</p>
         <img className={styles.image} src="./assets/img/perry.gif" />
+        {isCopied && (
+          <div className={styles.alerts}>
+            <p className={styles.alert}>Text copied to clipboard!</p>
+            <p className={styles.alert2}>Text copied to clipboard!</p>
+          </div>
+        )}
         <Input setIsCopied={setIsCopied} />
       </section>
-      {isCopied && (
-        <div className={styles.alerts}>
-          <p className={styles.alert}>Text copied to clipboard!</p>
-          <p className={styles.alert2}>Text copied to clipboard!</p>
-        </div>
-      )}
       <div className={styles.footer}>
         <p className={styles.under}>By @edv4ldez11</p>
         <p className={styles.below}>By @edv4ldez11</p>
